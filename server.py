2dcdd639d69449e5bc87f5f9c34866ad407dc88b
easonjan <a13564674280@163.com>
2023-01-30 19:07:19 +0800

    代码优化

--- server.py
+++ server.py
@@ -25,19 +25,21 @@ def receive(data):
     if data.get('message')[0] == ".":
         data['message'] = data['message'][1:]
         # print(data)
-        if  data['message'] == "vpn" or data['message'] == "vpn_all":    
+        if data['message'] == "vpn" or data['message'] == "vpn_all":
             if config['vpn']['attitude']:
                 vpn(data)
             else:
                 mes = config['messages']['vpn_off']
                 send_new(data, mes)
 
-def log(mes,out=True,write=True,type='info'):
+
+def log(mes, out=True, write=True, type='info'):
     if not os.path.exists('logs'):
         os.makedirs('logs')
     mes = f'[{datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")}] [{type}] {mes}'
     if write:
-        open(f'./logs/{time.strftime("%Y-%m-%d.txt",time.localtime())}','a',encoding='utf-8').write(f'{mes}\n')
+        open(f'./logs/{time.strftime("%Y-%m-%d.txt",time.localtime())}',
+             'a', encoding='utf-8').write(f'{mes}\n')
     if out:
         print(mes)
 
@@ -45,6 +47,7 @@ def log(mes,out=True,write=True,type='info'):
 def send(URL, data):
     requests.post(f"{config['config']['send_address']}{URL}", data)
     log(f"[post_send] {config['config']['send_address']}{URL} {data}")
+
 
 def send_new(datas, mes):
     if datas['message_type'] == 'group':
@@ -70,34 +73,22 @@ def vpn(data):
     data_vpn2 = json.loads(requests.get(config['vpn']['request']['getSubscribe_path'],
                            headers=config['vpn']['request']['getSubscribe_heasers']).text)['data']
 
-    if data_vpn2['plan']['month_price']:
-        ran = "月"
-        money = f"{float(data_vpn2['plan']['month_price'])/100}"
 
-    if data_vpn2['plan']['quarter_price']:
-        ran = "季"
-        money = f"{float(data_vpn2['plan']['quarter_price'])/100}"
-
-    if data_vpn2['plan']['half_year_price']:
-        ran = "半年"
-        money = f"{float(data_vpn2['plan']['half_year_price'])/100}"
-
-    if data_vpn2['plan']['year_price']:
-        ran = "年"
-        money = f"{float(data_vpn2['plan']['year_price'])/100}"
-
-    if data_vpn2['plan']['two_year_price']:
-        ran = "两年"
-        money = f"{float(data_vpn2['plan']['two_year_price'])/100}"
-
-    if data_vpn2['plan']['three_year_price']:
-        ran = "三年"
-        money = f"{float(data_vpn2['plan']['three_year_price'])/100}"
-
-    if data_vpn2['plan']['onetime_price']:
-        ran = "单次"
 
     if data['message'] == 'vpn_all':
+        price_plans = [
+        ("月", "month_price"),
+        ("季", "quarter_price"),
+        ("半年", "half_year_price"),
+        ("年", "year_price"),
+        ("两年", "two_year_price"),
+        ("三年", "three_year_price"),
+        ("单次", "onetime_price"),
+        ]
+        for ran, price_plan in price_plans:
+            if data_vpn2['plan'][price_plan]:
+                money = f"{float(data_vpn2['plan'][price_plan])/100}"
+                break
         mes = eval(config['messages']['vpn_all'])
 
     if data['message'] == 'vpn':
@@ -130,7 +121,7 @@ def vpn_alarm():
                             'message': eval(config['messages']['vpn_twentyu_alarm'])}
                     send("send_group_msg", data)
             last_u = u
-            time.sleep(1200)#1200
+            time.sleep(1200)  # 1200
 
 
 if __name__ == '__main__':
